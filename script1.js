26876018dd56f9c71780f7078bdabfe95f8e1990
AynanshAadyant <aynanshaadyant0001@gmail.com>
2024-04-14 19:16:01 +0530

    added new function PopulaterResult() that is responsible to display displayVal in calculator

--- script1.js
+++ script1.js
@@ -1,43 +1,51 @@
+
 let displayVal="";
 let calcVal="";
 function display(value) //displays the entered operations on the display tab
 {
-    if(value=='**2')
+    "use strict";
+    if(value === '**2')
     {
-        displayVal+="^2";
-        calcVal+=value;
+        displayVal += "^2";
+        calcVal += value;
+        PopulateResult(displayVal);
+        return;
     }
-    else if(value=='**') 
+    if(value === '**') 
     {
-        displayVal+="^";
-        calcVal+=value;
+        displayVal += "^";
+        calcVal += value;
+        PopulateResult(displayVal);
+        return;
     }
-    else
-    {
-        displayVal+=value;
-        calcVal+=value;
-    }
-    document.getElementById("display").innerHTML=displayVal;
-   
+    
+        displayVal += value;
+        calcVal += value;
+        PopulateResult(displayVal);
+    
+}
+
+function PopulateResult(inputValue)
+{
+    document.getElementById("display").innerHTML=inputValue;
+    //$("display").val(inputValue); =>
 }
 
 function clearDisplay()  //clears the result tab from previous operations
 {
-     displayVal="";calcVal="";
-     document.getElementById("display").innerHTML=displayVal;
+     displayVal = "";calcVal = "";
+     PopulateResult(displayVal);
 }
 
 function result()
 {
     try{
-        const res=eval(calcVal);
-        document.getElementById("display").innerHTML=res;
-        displayVal=res.toString();
-        calcVal=res.toString();
+        let res = eval(calcVal);
+        PopulateResult(res);
     }
     catch(error)
     {
-        document.getElementById("display").innerHTML="ERROR";
-        displayVal="";calcVal="";
+        PopulateResult("ERROR :" +error);
+        clearDisplay();
     }
 }
